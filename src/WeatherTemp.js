a80839ac22377173e3c5cd9362d1c62e9d3b2989
PaulinaBraz <124574292+PaulinaBraz@users.noreply.github.com>
2023-04-05 10:01:51 +0100

    Unit Conversion Fix

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -2,43 +2,43 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
-
-  function convertToFahrenheit(event) {
+  function changeToFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
 
-  function convertToCelsius(event) {
-    {
-      event.preventDefault();
-      setUnit("celsius");
-    }
+  function changeToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
 
-    if (unit === "celsius") {
-      return (
-        <div className="WeatherTemperature">
-          <span className="temperature">{Math.round(props.celsius)}</span>
-          <span className="unit">
-            °C|{" "}
-            <a href="/" onClick={convertToFahrenheit}>
-              °F{" "}
-            </a>
-          </span>
-        </div>
-      );
-    } else {
-      let fahrenheit = (props.celsius * 9) / 5 + 32;
-      return (
-        <div className="WeatherTemperature">
-          <span className="temperature">{Math.round(fahrenheit)}</span>
-          <span className="unit">
-            <a href="/" onClick={convertToCelsius}>
-              °C
-            </a>{" "}
-            | °F
-          </span>
-        </div>
-      );
-    }
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={changeToFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={changeToCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
   }
 }
